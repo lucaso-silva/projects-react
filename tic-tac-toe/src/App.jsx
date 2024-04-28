73a51bd34fef33878a4c85746a446f50a1fd621c
Lucas Oliveira <luc.osilva@gmail.com>
2024-04-27 19:58:10 -0700

    (wip) player component

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,19 @@
+import Player from "./components/Player"
+
+function App() {
+  
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player playerName="Player 1" symbol="X" />
+          <Player playerName="Player 2" symbol="O"/>          
+        </ol>
+      GAME BOARD
+      </div>
+    </main>
+  )
+}
+
+export default App
