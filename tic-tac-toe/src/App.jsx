2db001a47a91581a13204d7c3d727b832597a179
Lucas Oliveira <luc.osilva@gmail.com>
2024-04-28 22:41:37 -0700

    (wip) board game

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,16 +1,15 @@
-import Player from "./components/Player"
+import Player from "./components/Player.jsx"
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
-  
-
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player playerName="Player 1" symbol="X" />
-          <Player playerName="Player 2" symbol="O"/>          
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O"/>          
         </ol>
-      GAME BOARD
+        <GameBoard />
       </div>
     </main>
   )
--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,29 @@
-export default function Player({playerName, symbol}) {
+import { useState } from "react";
+
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleClick() {
+        // setIsEditing(!isEditing);  --> this schedules a state update
+        // correct way:
+        setIsEditing((editing)=>!editing);
+        
+    }
+
+    function handleInput(e) {
+        setPlayerName(e.target.value);
+    }
+
     return (
         <li>
             <span className="player">
-              <span className="player-name">{playerName}</span>
+              {!isEditing ? 
+                (<span className="player-name">{playerName}</span>) : 
+                (<input type="text" value={playerName} required onChange={handleInput}></input>)}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
           </li>
     )
 }
