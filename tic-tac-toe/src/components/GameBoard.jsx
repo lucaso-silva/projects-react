787aeadcbfc46fc11d6d9b84c1a3565e63dca18b
Lucas Oliveira <luc.osilva@gmail.com>
2024-04-29 21:15:36 -0700

    lifting state up

--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -1,15 +1,37 @@
+import {useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
 
-export default function GameBoard(){
+export default function GameBoard({onSelectSquare, activePlayerSymbol}){
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard)=>{
+            //This is not the correct way to change the array, instead we need to create a copy and then change this copy
+            // prevGameBoard[rowIndex][colIndex] = 'X';
+            // return prevGameBoard;
+
+            const updateGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updateGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updateGameBoard;
+        });
+
+        //we receive the function handleSelectSquare defined in the App component,
+        //and then call this function inside the GameBoardComponent
+        onSelectSquare();
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex)=><li key={rowIndex}>
+            {gameBoard.map((row, rowIndex)=><li key={rowIndex}>
                 <ol>
-                    {row.map((playerSymbol, colIndex)=><li key={colIndex}><button>{playerSymbol}</button></li>)}
+                    {row.map((playerSymbol, colIndex)=><li key={colIndex}>
+                                                                    <button onClick={()=>handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                                                </li>)}
                 </ol>
             </li>)}
         </ol>
