6b0b7ccb392611afe7c027a8de726bd64cf830de
Lucas Oliveira <luc.osilva@gmail.com>
2024-04-30 23:10:44 -0700

    (wip) log movements

--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -1,28 +1,34 @@
-import {useState } from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}){
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({onSelectSquare, turns}){
+    //const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard)=>{
-            //This is not the correct way to change the array, instead we need to create a copy and then change this copy
-            // prevGameBoard[rowIndex][colIndex] = 'X';
-            // return prevGameBoard;
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard)=>{
+    //         //This is not the correct way to change the array, instead we need to create a copy and then change this copy
+    //         // prevGameBoard[rowIndex][colIndex] = 'X';
+    //         // return prevGameBoard;
+    //
+    //         const updateGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updateGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updateGameBoard;
+    //     });
+    //
+    //     //we receive the function handleSelectSquare defined in the App component,
+    //     //and then call this function inside the GameBoardComponent
+    //     onSelectSquare();
+    // }
+    let gameBoard = initialGameBoard;
 
-            const updateGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updateGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updateGameBoard;
-        });
+    for(const turn of turns) {
+       const { square, player } = turn;
+       const {row, col} = player;
 
-        //we receive the function handleSelectSquare defined in the App component,
-        //and then call this function inside the GameBoardComponent
-        onSelectSquare();
+       gameBoard[row][col] = player;
     }
 
     return (
@@ -30,7 +36,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol}){
             {gameBoard.map((row, rowIndex)=><li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex)=><li key={colIndex}>
-                                                                    <button onClick={()=>handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                                                    <button onClick={()=> onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                                                                 </li>)}
                 </ol>
             </li>)}
